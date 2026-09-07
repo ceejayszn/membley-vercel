7ccf24e460f61f44d0e972b7934a5703248eb62e
ceejayszn <ceejayszn@github.com>
2026-09-07 11:58:40 +0300

    Fix PostgreSQL prepared statement error for connection poolers

--- includes/db.php
+++ includes/db.php
@@ -16,6 +16,7 @@ try {
         $pdo = new PDO($dsn, $user, $pass, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => true,
             PDO::ATTR_TIMEOUT => 5
         ]);
     } else {
